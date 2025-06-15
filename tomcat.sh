bc26d31e6d6877325e2df4482219fb117b2a1d8e
Saikiran-Chanda <167888508+Saikiran-Chanda@users.noreply.github.com>
2025-06-15 08:18:20 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -5,7 +5,7 @@ sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.98/conf/tomcat-u
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.98/conf/tomcat-users.xml
 sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.98/conf/tomcat-users.xml
 sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.98/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-9.0.98/conf/tomcat-users.xml
+sed -i '55d' apache-tomcat-9.0.98/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-9.0.98/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-9.0.98/webapps/manager/META-INF/context.xml
+sed -i '2dd'  apache-tomcat-9.0.98/webapps/manager/META-INF/context.xml
 sh apache-tomcat-9.0.98/bin/startup.sh
